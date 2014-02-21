1917c159314838d68e6dca17c4ffd37f699103d1
certainty <certainty@localhost>
2014-02-21 09:29:02 +0100

    simplified invokation protocol. seperated responsibilities by splitting making run-verification return the result and hand that over to the notify procedure

--- veritas.scm
+++ veritas.scm
@@ -4,11 +4,15 @@
   (require-library matchable kvlists)
   (import-for-syntax matchable)
 
+(define current-meta-data (make-parameter '()))
+
 (define-record verification-subject quoted-expression expression-promise meta-data)
 
 (define-record verification-result subject message status)
-(define (fail subject message) (make-verification-result subject message 'fail))
-(define (pass subject) (make-verification-result subject "" 'pass))
+
+(define (fail    subject message) (make-verification-result subject message 'fail))
+(define (pass    subject) (make-verification-result subject "" 'pass))
+(define (pending subject) (make-verification-result subject "" 'pending))
 
 (define (verification-failure? result)
   (and (verification-result? result)
@@ -18,10 +22,10 @@
   (and (verification-result? result)
        (eq? 'pass (verification-result-status result))))
 
+(define (verification-pending? result)
+  (and (verification-result? result)
+       (eq? 'pending (verification-result-status result))))
 
-(define pending? (make-parameter #f))
-(define current-description (make-parameter #f))
-(define current-meta-data (make-parameter '()))
 
 ;; The base library assumes nothing about outputting/handling  failed or succeeded verifications.
 ;; All it does is provide a protocoll that other parts can hook into to actually do something useful with this information
@@ -29,26 +33,14 @@
 (define current-success-notification-receiver (make-parameter (constantly #t)))
 (define current-pending-notification-receiver (make-parameter (constantly #t)))
 
-;; support procedures for reporter
-(define reporter-use-colors?
-  (make-parameter
-   (cond
-    ((get-environment-variable "VERITAS_USE_COLORS")
-     => (lambda (s) (not (equal? s "0"))))
-    (else
-     (and (##sys#tty-port? (current-output-port))
-          (member (get-environment-variable "TERM")
-                  '("xterm" "xterm-color" "xterm-256color" "rxvt" "kterm"
-                    "linux" "screen" "screen-256color" "vt100")))))))
-
 (define (notify-failure result)
   ((current-failure-notification-receiver) result))
 
 (define (notify-success result)
   ((current-success-notification-receiver) result))
 
-(define (notify-pending subject)
-  ((current-pending-notification-receiver) subject))
+(define (notify-pending result)
+  ((current-pending-notification-receiver) result))
 
 (define (notify result)
   (cond
@@ -56,8 +48,12 @@
     (notify-failure result))
    ((verification-success? result)
     (notify-success result))
-   (else (error "invalid result" result))))
+   ((verification-pending? result)
+    (notify-pending result))
+   (else (error "invalid result" result)))
+  result)
 
+;; META-DATA
 (define (merge-alists lhs rhs)
   (fold (lambda (elt ls) (alist-update (car elt) (cdr elt) ls)) lhs rhs))
 
@@ -67,13 +63,40 @@
 (define (meta-data-get subject key)
   (alist-ref key (verification-subject-meta-data subject)))
 
-(define-syntax meta
-  (syntax-rules ()
-    ((_ (k v ...) body0 ...)
-     (parameterize ((current-meta-data (merge-meta-data (quote (k v ...)))))
-       body0 ...))))
+
+;; this little indirection is here to have control over
+;; how/if tests are run.
+;; for example one might want to run them in a sandbox
+;; or in its own thread
+(define (run-verification subject verifier complement?)
+  (if (meta-data-get subject 'pending)
+      (pending subject)
+      (apply-verifier subject verifier complement?)))
+
+(define (apply-verifier subject verifier complement?)
+  ;; add timing and error handling
+  (condition-case (verifier subject complement?)
+    (e () (condition->verification-failure e))))
+
+(define (condition->verification-failure condition) #t)
+
+;; the verifier protocol is simple
+;; a verifier is a procedure that returns a procedure of two arguments
+;; 1) subject - the verification subject
+;; 2) complement? - is that in complement context
+(define ((boolean-verifier . args) subject complement?)
+  (let* ((expr (verification-subject-expression-promise subject))
+         (quoted-expr (verification-subject-quoted-expression subject))
+         (result (if complement? (not (force expr)) (force expr))))
+    (if result
+        (pass subject)
+        (fail subject
+              (if complement?
+                  (sprintf "Expected ~S not to hold" (cadr quoted-expr))
+                  (sprintf "Expected ~S to hold" (cadr quoted-expr)))))))
 
 
+;; SYNTAX
 ;; TODO: find a way to generate both verify and falsify with only one syntax
 (define-syntax verify
   (ir-macro-transformer
@@ -84,13 +107,14 @@
        ((_ expr (? string? description))
         `(verify ,expr (boolean-verifier) ,description))
        ((_ expr verifier)
-        `(run-verification
-          (make-verification-subject
-           (quote (verify ,expr ,verifier))
-           (delay ,expr)
-           (current-meta-data))
-          ,verifier
-          #f))
+        `(notify
+          (run-verification
+           (make-verification-subject
+            (quote (verify ,expr ,verifier))
+            (delay ,expr)
+            (current-meta-data))
+           ,verifier
+           #f)))
        ((_ expr verifier (? string? description))
         `(meta (description: ,description)
            (verify ,expr ,verifier)))
@@ -106,13 +130,14 @@
        ((_ expr (? string? description))
         `(falsify ,expr (boolean-verifier) ,description))
        ((_ expr verifier)
-        `(run-verification
-          (make-verification-subject
-           (quote (falsify ,expr ,verifier))
-           (delay ,expr)
-           (current-meta-data))
-          ,verifier
-          #t))
+        `(notify
+          (run-verification
+           (make-verification-subject
+            (quote (falsify ,expr ,verifier))
+            (delay ,expr)
+            (current-meta-data))
+           ,verifier
+           #t)))
        ((_ expr verifier (? string? description))
         `(meta (description: ,description)
            (falsify ,expr ,verifier)))
@@ -148,39 +173,11 @@
      (meta (description: description)
          body0 ...))))
 
-;; this little indirection is here to have control over
-;; how/if tests are run.
-;; for example one might want to run them in a sandbox
-;; or in its own thread
-(define (run-verification subject verifier complement?)
-  (if (meta-data-get subject 'pending)
-      (notify-pending subject)
-      (let ((result (apply-verifier subject verifier complement?)))
-        (if (verification-failure? result)
-            (notify-failure result)
-            (notify-success result))
-        result)))
+(define-syntax meta
+  (syntax-rules ()
+    ((_ (k v ...) body0 ...)
+     (parameterize ((current-meta-data (merge-meta-data (quote (k v ...)))))
+       body0 ...))))
 
-(define (apply-verifier subject verifier complement?)
-  ;; add timing and error handling
-  (condition-case (verifier subject complement?)
-    (e () (condition->verification-failure e))))
-
-(define (condition->verification-failure condition) #t)
-
-;; the verifier protocol is simple
-;; a verifier is a procedure that returns a procedure of two arguments
-;; 1) subject - the verification subject
-;; 2) complement? - is that in complement context
-(define ((boolean-verifier . args) subject complement?)
-  (let* ((expr (verification-subject-expression-promise subject))
-         (quoted-expr (verification-subject-quoted-expression subject))
-         (result (if complement? (not (force expr)) (force expr))))
-    (if result
-        (pass subject)
-        (fail subject
-              (if complement?
-                  (sprintf "Expected ~S not to hold" (cadr quoted-expr))
-                  (sprintf "Expected ~S to hold" (cadr quoted-expr)))))))
 
 )
