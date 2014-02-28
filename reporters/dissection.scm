a599ce80175963912e7f0197b0b81d6eec230caf
certainty <certainty@localhost>
2014-02-28 14:42:28 +0100

    added dissection reporter

--- reporters/dissection.scm
+++ reporters/dissection.scm
@@ -1,2 +1,14 @@
-;; add a reporter that halts at each failure and
-;; starts the dissector with subject
+(module veritas-dissection-reporter
+  ()
+  (import chicken scheme)
+  (use veritas veritas-base-reporter dissector)
+
+  (define dissect-every-failure (make-parameter #f))
+
+  (define (failure-handler result)
+    (let ((subject (verification-result-subject result)))
+      (when (or (dissect-every-failure) (meta-data-get subject 'dissect))
+        (parameterize ((dissection-prompt "Dissector>> "))
+          (dissect (force (verification-subject-expression-promise subject)))))))
+
+  (add-failure-listener failure-handler))
